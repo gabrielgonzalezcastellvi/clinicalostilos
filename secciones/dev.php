75a0b703f5b4e79a2a91702ef1706f48e09b8833
Gabriel Gonzalez Castellvi <50787949+gabrielgonzalezcastellvi@users.noreply.github.com>
2023-08-21 23:16:24 -0300

    .ar

--- secciones/dev.php
+++ secciones/dev.php
@@ -1,4 +1,4 @@
 <footer class="tiny">
 <p><?php echo date("Y") . ' ' . BRAND; ?></p>
-<p>Desarrollado por <a href="https://somoswizard.com" target="_blank" rel="noopener noreferrer" class="hover-shrink">Wizard<i class="bi bi-whatsapp"></i></a></p>
+<p>Desarrollado por <a href="https://somoswizard.com.ar" target="_blank" rel="noopener noreferrer" class="hover-shrink">Wizard<i class="bi bi-whatsapp"></i></a></p>
 </footer>
